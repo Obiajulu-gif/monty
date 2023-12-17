19a637882b521469febf232c152f3e80a22f6fd6
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2023-12-17 10:42:10 +0100

    push and pall done

--- monty.c
+++ monty.c
@@ -1,15 +1,13 @@
 #include  "monty.h"
 
+int main(void)
+{
+Stack stack;
+stack.top = 0;
+push(&stack, 1);
+push(&stack, 2);
+push(&stack, 3);
+pall(&stack);
 
-
-int main() {
-    Stack stack;
-    stack.top = 0;
-
-    push(&stack, 1);
-    push(&stack, 2);
-    push(&stack, 3);
-    pall(&stack);
-
-    return 0;
+return (0);
 }
--- push_pall.c
+++ push_pall.c
@@ -6,21 +6,25 @@
  * @stack: The stack.
  * @value: The value to push.
  */
-void push(Stack *stack, int value) {
-    if (stack->top == STACK_SIZE) {
-        fprintf(stderr, "Stack overflow\n");
-        exit(EXIT_FAILURE);
-    }
-    stack->stack[stack->top++] = value;
+void push(Stack *stack, int value)
+{
+if (stack->top == STACK_SIZE)
+{
+fprintf(stderr, "Stack overflow\n");
+exit(EXIT_FAILURE);
+}
+stack->stack[stack->top++] = value;
 }
 
 /**
  * pall - Prints all the values on the stack, starting from the top.
  * @stack: The stack.
  */
-void pall(Stack *stack) {
-    int i;
-    for (i = stack->top - 1; i >= 0; i--) {
-        printf("%d\n", stack->stack[i]);
-    }
+void pall(Stack *stack)
+{
+int i;
+for (i = stack->top - 1; i >= 0; i--)
+{
+printf("%d\n", stack->stack[i]);
+}
 }
